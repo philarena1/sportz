ab8aa6952d7bc40753edc92df91e1cc3df930c57
Philip Arena <philiplarena@gmail.com>
2019-03-10 19:43:58 -0400

    added function to get date from file name

--- read_lines.py
+++ read_lines.py
@@ -2,9 +2,13 @@ import glob
 import pandas as pd
 import os
 
+# get all csv files in folders
 path = r'venv/daily_lines'                     # use your path
 all_files = glob.glob(os.path.join(path, "*.csv"))
 
+# read all csv and make 1 df
 df_from_each_file = (pd.read_csv(f,index_col=False ) for f in all_files)
-all_lines_df   = pd.concat(df_from_each_file, ignore_index=True,sort=False)
+all_lines_df = pd.concat(df_from_each_file, ignore_index=True,sort=False)
 
+# get date from file name
+all_lines_df['day'] = all_lines_df.file_name.str.split("_").str[2]
